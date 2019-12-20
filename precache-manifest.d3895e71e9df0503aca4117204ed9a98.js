2bf2eaf6a749ad9579884f6e3a721e60127ddccd
jambojuice <playir77@gmail.com>
2019-12-20 23:07:21 +0100

    deploy

---
+++ precache-manifest.d3895e71e9df0503aca4117204ed9a98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11040f0e5694ee89fdd4",
+    "url": "/debate-stopwatch/css/app.e07744a4.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/debate-stopwatch/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3253f5310f2d03c00e6101074b9b105d",
+    "url": "/debate-stopwatch/index.html"
+  },
+  {
+    "revision": "916f6d6e5108828f3627",
+    "url": "/debate-stopwatch/js/about.61382c5a.js"
+  },
+  {
+    "revision": "11040f0e5694ee89fdd4",
+    "url": "/debate-stopwatch/js/app.da5c8107.js"
+  },
+  {
+    "revision": "e54845ab283cf892741c",
+    "url": "/debate-stopwatch/js/chunk-vendors.98f3cc2e.js"
+  },
+  {
+    "revision": "544e652062045284486be5a3ceb0299a",
+    "url": "/debate-stopwatch/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/debate-stopwatch/robots.txt"
+  }
+]);
